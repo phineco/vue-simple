ef40221a41e3a2106e682d73a104336fb147625f
Louis Wang <phineco@gmail.com>
2019-04-20 12:05:32 +0800

    修改样式，使用vue-datepick选择时间

--- src/pages/activity/register.vue
+++ src/pages/activity/register.vue
@@ -3,77 +3,91 @@
     <div class="headerDiv">
       待筛查人员信息
     </div>
-    <div class="login_container">
+    <div class="login_container" v-if="isDisplay">
 
-      <mt-field label="姓名" placeholder=""
-                v-model="name"
-                v-verify="name">
-      </mt-field>
-      <mt-field label="性别" placeholder=""
-                disabled >
-        <mt-switch :class='getGender' v-model='isMale'>{{sex}}</mt-switch>
-      </mt-field>
-      <div @click.stop="openPicker" class="disableInput" ref="disableInput">
-        <mt-field label="出生日期" placeholder=""
-                  v-model="selectedValue"
-                  v-verify="selectedValue">
-        </mt-field>
-      </div>
+      <ul class="list">
+        <li >
+          <div>
+            <div class="fr-text"><input v-model="name" placeholder="请输入姓名" v-verify="name"/></div>
+            <div class="nav-name" >姓名</div>
+          </div>
+          </li>
+        <li>
+          <div>
+            <div class="fr-text"><mt-switch :class='getGender' v-model='isMale'>{{sex}}</mt-switch></div>
+            <div class="nav-name" >性别</div>
+          </div>
+        </li>
+        <li>
+          <div>
+            <div class="fr-text">
+              <input type="text" onfocus="this.blur()" @click = "setDate" v-bind:value="selectedValue" placeholder="请选择出生日期"/>
+            </div>
+            <div class="nav-name" >出生日期</div>
+          </div>
+        </li>
 
-      <mt-field label="随访联系电话" placeholder="可选填"
-                v-model="contact"
-                v-verify="contact">
-      </mt-field>
+
+        <li>
+          <div >
+            <div class="fr-text"><input v-model="contact" placeholder="" v-verify="contact"/></div>
+            <div class="nav-name" >随访联系电话</div>
+          </div>
+        </li>
+
+
+      </ul>
 
       <div class="bind-btn" @click.stop="register">提    交</div>
 
       <p class="examP">注:请务必填写现场待抽血人员的实际信息!</p>
     </div>
+    <div v-else class="introduce">
+      <div>
+        请参照以下步骤进行报名:
+        <p>
+          1. 扫描关注联盟微信公众号
+        </p>
+        <p>
+          2. 在公众号菜单里选择"GECA服务",进入"个人中心"
+        </p>
+        <p>
+          3. 绑定手机号
+        </p>
+        <p>
+          4. 扫描筛查报名二维码,填入报名信息
+        </p>
+        <p>
+          5. 将生成的筛查编号出示给工作人员记录
+        </p>
+      </div>
 
-
-    <div class="pickerPop" @touchmove.prevent>
-      <mt-datetime-picker
-        ref="picker"
-        type="date"
-        v-model="testDate"
-        :endDate="endDate"
-        :startDate="startDate"
-        lockScroll="true"
-        class="myPicker"
-        year-format="{value}"
-        month-format="{value}"
-        date-format="{value}"
-        @confirm="dateConfirm()"
-      >
-      </mt-datetime-picker>
     </div>
-
   </div>
 </template>
 
 <script type="application/ecmascript">
   import { LOGIN } from '@/store/types';
   import {sendVerifyCode,sendLogin} from '../../js/api';
-  import { DatetimePicker } from 'mint-ui';
-  import {formatDate} from '../../js/mUtils';
+  import {formatDate, getCookieByName} from '../../js/mUtils';
   import { MessageBox } from "mint-ui";
   import verify from 'verify-plugin';
-  Vue.component(DatetimePicker.name, DatetimePicker)
+  import { Toast } from 'mint-ui';
+
 
   export default {
     name: '',
     data() {
       return {
+        isDisplay:true,
         name: null,
         isMale: false,
         sex:"男",
         mobile:null,
         contact:null,
-        selectedValue:null,
+        selectedValue:"1980-01-01",
         testDate:null,
-        startDate:new Date('1945-01-01'),
-        endDate:new Date('1985-12-31'),
-        examId:null
+        examId:null,
       };
     },
     verify: {
@@ -84,6 +98,7 @@
         }
       ],
       selectedValue: [
+        'number',
         {
           test: 'required',
           message: '出生日期不能为空'
@@ -94,8 +109,9 @@
       ],
     },
     mounted() {
-      //console.log(this.$refs.disableInput.querySelector('input'))
-      this.$refs.disableInput.querySelector('input').setAttribute("disabled","disabled");
+      if (getCookieByName("mxgs_token") != "") {
+        this.isDisplay = true;
+      }
     },
     computed: {
       getGender: function() {
@@ -106,44 +122,32 @@
         }
 
       }
-      ,
-      isDisabled: function () {
-        return !(this.$verify.validate('name', true)
-          && this.$verify.validate('selectedValue', true)
-        );
-
-      }
     },
     methods: {
-      openPicker () { // 打开时间选择器
-        // 如果已经选过日期，则再次打开时间选择器时，日期回显（不需要回显的话可以去掉 这个判断）
-        if (this.selectedValue) {
-          this.testDate = this.selectedValue;
-        } else {
-          this.testDate = new Date('1980-01-01');
-        }
-        this.$refs.picker.open();
-      },
-      dateConfirm () { // 时间选择器确定按钮，并把时间转换成我们需要的时间格式
-        //alert(this.testDate instanceof Date);
-        if (this.testDate instanceof Date) {
-          //如果打开日历但没有选择日期时,不会返回值
-          this.selectedValue = formatDate(this.testDate, 'yyyy-MM-dd');
-        }
-      },
-
+      setDate(){
+        this.$picker.show({
+          type:'datePicker',
+          year:'1980',
+          month:'1',
+          day:'1',
+          onOk: (date) =>{
+          this.selectedValue = date
+      }
+      })},
       register() {
         if (!this.$verify.check()){
           return;
           }
+
           let url = "/f/register"
         this.axios.post(url, {"activityId": 1,
             hospitalId: this.$route.query.hospitalId,
             name: this.name,
             sex: this.sex,
-            birthday: this.testDate,
+            birthday: this.selectedValue,
             mobile: this.mobile,
             contact: this.contact,
+            age: this.age,
             isSync: 0
           }
           )
@@ -151,23 +155,19 @@
 
           MessageBox.alert("保存成功");
         this.examId = response.data.examId;
+        this.hospitalId = response.data.hospitalId;
         console.log("ok, id=" + this.examId);
         this.$router.replace({
           path:'/userInfo',
           query:{
-            examId : this.examId
+            examId : this.examId,
+            hospitalId: this.hospitalId
           }
         });
       }).catch(error => {
           console.log("error" + error)
         MessageBox.alert("保存失败,请重试");
       });
-        /*
-         MessageBox.prompt(' ', '请输入标题').then(({ value }) => {
-         if (value) {
-         MessageBox.alert(`请输入标题是 ${ value }`, '输入成功');
-         }
-         });*/
       }
     }
   };
@@ -177,8 +177,6 @@
 <style lang="scss" scoped>
   @import 'src/css/style';
   .login_container {
-    margin-left: r(40);
-    margin-right: r(40);
     div, p, span {
       font-family: Helvetica Neue, Tahoma, Arial;
       font-size: $main_font_size;
@@ -195,21 +193,8 @@
          }
   }
 
-
-  .pickerPop{
-  /deep/ .picker{
-  .picker-toolbar{
-    background-color: #eee;
-  .mint-datetime-action{
-    font-size: 1rem;
-    color: #000!important;
-  }
-  }
-  }
-  }
-
   .bind-btn{
-    width:r(690);
+    width:r(670);
     height:r(90);
     margin:r(50) auto 0;
     border-radius:r(6);
@@ -230,12 +215,70 @@
     background-color: $secondary_text_color;
   }
 
-  .disableInput{
-  /deep/ .mint-field-core {
-    readonly:true;
-         }
-   input{
-     color: #000000;
-   }
+.list{
+  background-color:#ffffff;
+  li {
+    margin-left: r(20);
+    margin-right: r(20);
+    overflow: hidden;
+    border-bottom: 1px solid #f2f2f2;
+    padding: r(20) r(35) r(20) 0;
+    position: relative;
+    line-height:r(60);
+  .icon-box1 {
+    width: r(55);
+    height: r(55);
+    float: left;
+  img {
+    width: 100%;
   }
+  }
+  .icon-box {
+    width: r(32);
+    height: r(32);
+    float: left;
+    padding-right:r(10);
+  img {
+    width: 100%;
+  }
+  }
+  .fr-text {
+    float: right;
+    font-size: $main_font_size;
+    line-height: r(60);
+    padding-top: r(5);
+    span {
+      padding-top: r(5);
+      padding-right: r(30);
+    }
+    input {
+      font-size: $main_font_size;
+      line-height: r(50);
+      border: 1px;
+    }
+  img {
+    height: r(36);
+    width: r(20);
+  }
+  }
+  .nav-name {
+    font-size: $main_font_size;
+    color: $primary_text_color;
+    line-height: r(60);
+  }
+  }
+  }
+  .introduce{
+    font-family: Helvetica Neue, Tahoma, Arial;
+    margin:20px auto 0;
+    div {
+      font-size: $large_font_size;
+      padding: r(20);
+    }
+    p {
+      padding: r(10);
+      font-size: $main_font_size
+    }
+  }
+
 </style>
